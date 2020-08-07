190eccae810db9cfcf348167e1c93898a6614c75
Daniel Turner <danielturner184@gmail.com>
2020-08-07 09:09:37 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,8 +1,7 @@
-# Write your methods here
-def loop_message_five_times[message]
-  count = 0 
-  while count 5 < do
+def loop_message_five_times(message)
+  count = 0
+  while count < 5 do
     puts message
-    count += 1 
-  end 
+    count += 1
+  end
 end
