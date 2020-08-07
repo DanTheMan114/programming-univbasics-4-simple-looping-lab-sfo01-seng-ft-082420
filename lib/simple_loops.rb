8e46c0117850ecccd667b7995446dcff3d4b0352
Daniel Turner <danielturner184@gmail.com>
2020-08-07 09:08:28 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,2 +1,8 @@
 # Write your methods here
-def loop_message_five_times(Hello World)
+def loop_message_five_times["Hello World."]
+  count = 0 
+  while count 5 < do
+    puts "Hello World."
+    count += 1 
+  end 
+end
